e574fec6a485aab0ee9327db6bdaf430d18a0be1
Robert Zuniga <github@mastertheday.com>
2019-09-01 23:15:34 -0400

    update

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -12,13 +12,16 @@ function Navbar() {
         <div className="navbar-collapse collapse dual-nav w-50 order-1 order-md-0">
             <ul className="navbar-nav">
                 <li className="nav-item active">
-                    <a className="nav-link pl-0 display-4" href="/">Clicky Game<span className="sr-only">Clicky Game</span></a>
+                    <a className="nav-link pl-0 brand " href="/">Clicky Game<span className="sr-only">Clicky Game</span></a>
                 </li>
             </ul>
         </div>
+
         <a href="/" className="navbar-brand mx-auto d-block text-center order-0 order-md-1 w-25">Click an image to begin!</a>
+ 
         <div className="navbar-collapse collapse dual-nav w-50 order-2">
             <ul className="nav navbar-nav ml-auto">
+ 
                 <li className="nav-item"><a className="nav-link" href="/"><i className="fa fa-twitter"></i></a></li>
  
             </ul>
